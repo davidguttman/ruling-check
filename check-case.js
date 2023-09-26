b06afe242b1a418f01fdf6681000de9218bd50ac
David Guttman <david@davidguttman.com>
2023-09-26 15:17:01 -0700

    style: make standard happy

--- check-case.js
+++ check-case.js
@@ -1,34 +1,36 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer')
 
-async function checkCase(caseNumber) {
-    const browser = await puppeteer.launch({
-        headless: 'new'
-    });
-    const page = await browser.newPage();
+async function checkCase (caseNumber) {
+  const browser = await puppeteer.launch({
+    headless: 'new'
+  })
+  const page = await browser.newPage()
 
-    await page.goto('https://www.lacourt.org/tentativeRulingNet/ui/main.aspx?casetype=civil');
+  await page.goto(
+    'https://www.lacourt.org/tentativeRulingNet/ui/main.aspx?casetype=civil'
+  )
 
-    // Enter the case number into the search box
-    await page.type('input[name="CaseNumber"]', caseNumber);
+  // Enter the case number into the search box
+  await page.type('input[name="CaseNumber"]', caseNumber)
 
-    // Click the search button
-    await Promise.all([
-        page.waitForNavigation(),
-        page.click('input[id="submit2"]')
-    ]);
+  // Click the search button
+  await Promise.all([
+    page.waitForNavigation(),
+    page.click('input[id="submit2"]')
+  ])
 
-    // Check if the case was found
-    const caseFound = await page.evaluate(() => {
-        const noResultsElement = document.querySelector('#speechSynthesis');
-        return (
-            !noResultsElement ||
-            !noResultsElement.innerText.includes('No rulings found')
-        );
-    });
+  // Check if the case was found
+  const caseFound = await page.evaluate(() => {
+    const noResultsElement = document.querySelector('#speechSynthesis')
+    return (
+      !noResultsElement ||
+      !noResultsElement.innerText.includes('No rulings found')
+    )
+  })
 
-    await browser.close();
+  await browser.close()
 
-    return caseFound;
+  return caseFound
 }
 
-module.exports = checkCase;
+module.exports = checkCase
